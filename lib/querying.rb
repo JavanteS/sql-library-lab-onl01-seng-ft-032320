209b13e7c6d6be37ce05a15eacf0e1294869f5e8
Javante Nelson <javantenelson@gmail.com>
2020-04-28 14:47:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-   JOIN characters
+   INNER JOIN characters
    ON series.id = characters.series_id
    WHERE character.species = 'human' LIMIT 1"
 end
